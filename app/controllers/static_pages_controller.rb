5ec42c607a08c75c5de6d033d2b2e546cb76d17a
Leo Torres <leo.htor@gmail.com>
2017-07-02 21:57:19 -0400

    init commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,17 @@
 class StaticPagesController < ApplicationController
 
+require 'rubygems'
+require 'gems'
+
+
   def root
+  end
+
+  def show
+    @gems = Gems.search(params[:name])
+      respond_to do |format|
+        format.html
+      end
   end
 
   def favorites
